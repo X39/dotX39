20b775d94a239702aa5598c5a218131bf4a0da24
Marco.Silipo <m.silipo@web.de>
2015-04-22 23:28:12 +0200

    Fixed ArrayError and finished implementation 1.0

--- dotX39/main.cpp
+++ dotX39/main.cpp
@@ -3,11 +3,15 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
+#include "DataScalar.h"
+#include "DataString.h"
 
 void main()
 {
 	dotX39::Node* node = new dotX39::Node("root");
 	dotX39::DocumentReader::readDocument("D:\\GitHub\\dotX39\\dotX39\\example.x39", node);
+	node->addArgument(new dotX39::DataScalar(1, "argAddedAfterReading"));
+	node->addArgument(new dotX39::DataString("[]", "escapingTest"));
 	dotX39::DocumentWriter::writeDocument("D:\\GitHub\\dotX39\\dotX39\\output.x39", node);
 	delete node;
 	_CrtDumpMemoryLeaks();
