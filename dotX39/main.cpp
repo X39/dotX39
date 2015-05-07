5a8e60bad61bf9d2d8e5557d5ca5ff50471b6c0c
Marco.Silipo <m.silipo@web.de>
2015-05-07 18:21:32 +0200

    Fixed #1
    Root cause:
    When reading the string the DocumentReader automatically detects the
    char used but does not carries this over when the string continues in a
    new block of data which still needs to be received by the reader
    
    thus it tried to read to 't' for the string ..."foo| |tbar"... instead
    of reading to the "

--- dotX39/main.cpp
+++ dotX39/main.cpp
@@ -9,7 +9,8 @@
 void main()
 {
 	dotX39::Node* node = new dotX39::Node("root");
-	dotX39::DocumentReader::readDocument("D:\\GitHub\\dotX39\\dotX39\\example.x39", node);
+	//dotX39::DocumentReader::readDocument("D:\\GitHub\\dotX39\\dotX39\\example.x39", node);
+	dotX39::DocumentReader::readDocument("D:\\GitHub\\X39sTemplateReplace\\replaceTool\\proj.x39", node);
 	node->addArgument(new dotX39::DataScalar(1, "argAddedAfterReading"));
 	node->addArgument(new dotX39::DataString("[]", "escapingTest"));
 	dotX39::DocumentWriter::writeDocument("D:\\GitHub\\dotX39\\dotX39\\output.x39", node);
