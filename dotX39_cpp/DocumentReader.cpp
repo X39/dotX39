1d9dd879f61d83512b45cb2f7a8dd91afd90783b
Marco.Silipo <m.silipo@web.de>
2015-05-07 18:21:32 +0200

    Fixed #1
    Root cause:
    When reading the string the DocumentReader automatically detects the
    char used but does not carries this over when the string continues in a
    new block of data which still needs to be received by the reader
    
    thus it tried to read to 't' for the string ..."foo| |tbar"... instead
    of reading to the "

--- dotX39_cpp/DocumentReader.cpp
+++ dotX39_cpp/DocumentReader.cpp
@@ -67,6 +67,7 @@ namespace dotX39
 			{
 				while (!doc.eof())
 				{
+					char stringDetectionChar;
 					doc.read(s, READER_BUFFERSIZE - 1);
 					auto gcount = doc.gcount();
 					s[gcount] = '\0';
@@ -370,19 +371,15 @@ namespace dotX39
 									char* cp2;
 									char* cp3 = c + 1;
 									int j = 0;
-									char c2 = c[0];
+									if (data.empty())
+										stringDetectionChar = c[0];
 									while (true)
 									{
-										cp = strchr(cp3, c2);
+										cp = strchr(cp3, stringDetectionChar);
 										if (cp == NULL)
-										{
 											break;
-										}
-										for (cp2 = cp; cp2[-1] == '\\';)
-										{
+										for (cp2 = cp; cp2[-1] == '\\'; cp2--)
 											j++;
-											cp2 = cp2 - 1;
-										}
 										if (j % 2 != 1)
 											break;
 										cp3 = cp + 1;
