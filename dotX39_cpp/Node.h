9e0bea45f4e734950b5de268ab458cde6984bb66
Marco.Silipo <m.silipo@web.de>
2016-04-21 03:25:52 +0200

    Moved Node class fully into Node.h

--- dotX39_cpp/Node.h
+++ dotX39_cpp/Node.h
@@ -13,21 +13,74 @@ namespace dotX39
 		std::vector<Data*> _argList;
 		std::vector<Node*> _nodeList;
 	public:
-		Node(std::string s);
-		~Node();
+		Node(std::string s)
+		{
+			this->_name = (s.empty() ? "NOTSET" : s);
+		}
+		~Node()
+		{
+			while (!this->_argList.empty()){ delete this->_argList.back(); this->_argList.pop_back(); }
+			while (!this->_dataList.empty()){ delete this->_dataList.back(); this->_dataList.pop_back(); }
+			while (!this->_nodeList.empty()){ delete this->_nodeList.back(); this->_nodeList.pop_back(); }
+		}
 
-		void addSubnode(Node* obj);
-		void addArgument(Data* obj);
-		void addData(Data* obj);
-		const Data* getData(unsigned int index) const;
-		const std::vector<Data*>* getDataArray(void) const;
-		unsigned int getDataCount(void) const;
-		const Data* getArgument(unsigned int index) const;
-		const std::vector<Data*>* getArgumentArray(void) const;
-		unsigned int getArgumentCount(void) const;
-		const Node* getNode(unsigned int index) const;
-		const std::vector<Node*>* getNodeArray(void) const;
-		unsigned int getNodeCount(void) const;
-		std::string getName(void) const;
+		void addSubnode(Node* obj)
+		{
+			this->_nodeList.push_back(obj);
+		}
+		void addArgument(Data* obj)
+		{
+			this->_argList.push_back(obj);
+		}
+		void Node::addData(Data* obj)
+		{
+			this->_dataList.push_back(obj);
+		}
+		const std::vector<Data*>* Node::getDataArray(void) const
+		{
+			return &this->_dataList;
+		}
+		unsigned int Node::getDataCount(void) const
+		{
+			return this->_dataList.size();
+		}
+		const Data* Node::getData(unsigned int index) const
+		{
+			if (this->_dataList.size() <= index)
+				return NULL;
+			return this->_dataList[index];
+		}
+		const std::vector<Data*>* Node::getArgumentArray(void) const
+		{
+			return &this->_argList;
+		}
+		unsigned int Node::getArgumentCount(void) const
+		{
+			return this->_argList.size();
+		}
+		const Data* Node::getArgument(unsigned int index) const
+		{
+			if (this->_argList.size() <= index)
+				return NULL;
+			return this->_argList[index];
+		}
+		const std::vector<Node*>* Node::getNodeArray(void) const
+		{
+			return &this->_nodeList;
+		}
+		unsigned int Node::getNodeCount(void) const
+		{
+			return this->_nodeList.size();
+		}
+		const Node* Node::getNode(unsigned int index) const
+		{
+			if (this->_nodeList.size() <= index)
+				return NULL;
+			return this->_nodeList[index];
+		}
+		std::string Node::getName(void) const
+		{
+			return this->_name;
+		}
 	};
 };
